36d67624c082f8d1eee545705a22cd68fcdcbd82
kedar235 <kedar235@gmail.com>
2017-02-14 16:06:06 -0700

    Adding Remote MongoServer

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/HungryMe';
+var url = 'mongodb://serteam6:hungryme123@ds153179.mlab.com:53179/hungryme';
 
 app.use(bodyParser.json()); 
 
